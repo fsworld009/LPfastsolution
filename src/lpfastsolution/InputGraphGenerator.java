597456ab4d85802986eba30c1c8175ccd5803a4b
WorldFS <fsworld009@gmail.com>
2014-02-10 16:07:49 -0600

    generate link costs, k should be the least number of low cost nodes because it is impossible to let every node has exactly k low cost nodes

--- src/lpfastsolution/InputGraphGenerator.java
+++ src/lpfastsolution/InputGraphGenerator.java
@@ -19,27 +19,43 @@ public class InputGraphGenerator {
         int nextNode;
         int ix,jx;
         
+        ArrayList[] lowCostLink = new ArrayList[numOfNodes];
+        for(ix=0;ix<numOfNodes;ix++){
+            lowCostLink[ix] = new ArrayList();
+        }
         
+        for(ix=0;ix<numOfNodes;ix++){    
+            //pick kx links to be low cost link
+            System.out.printf("Node %d\n",ix);
+            while(lowCostLink[ix].size()<kx){
+                nextNode = random.nextInt(numOfNodes);
+                System.out.printf("%d ",nextNode);
+                if(nextNode != ix && !lowCostLink[ix].contains(nextNode)){
+                    System.out.printf("    pick node %d\n",nextNode);
+                    lowCostLink[ix].add(nextNode);
+                    lowCostLink[nextNode].add(ix);
+                }
+            }
+        }
         
-        
-        
-        for(ix=0;ix<numOfNodes;ix++){          
+
+        for(ix=0;ix<numOfNodes;ix++){              
             for(jx=ix;jx<numOfNodes;jx++){
                 if(ix==jx){
                     demand[ix][jx]=0;
                     cost[ix][jx]=0;
                 }else{
                     demand[ix][jx]=random.nextInt(4);
-                    /*if(lowCostLink.contains(jx)){
-                        cost[ix][jx]=1;
+                    demand[jx][ix] = demand[ix][jx];
+                    if(lowCostLink[ix].contains(jx) || lowCostLink[jx].contains(ix)){
+                        cost[ix][jx] = 1;
+                        cost[jx][ix] = 1;
                     }else{
-                        cost[ix][jx]=300;
-                    }*/
+                        cost[ix][jx] = 300;
+                        cost[jx][ix] = 300;
+                    }
                 }
                 
-                //symmetry
-                demand[jx][ix] = demand[ix][jx];
-                cost[jx][ix] = cost[ix][jx];
             }
         }
         
@@ -53,7 +69,7 @@ public class InputGraphGenerator {
         System.out.println();
         for(ix=0;ix<numOfNodes;ix++){
             for(jx=0;jx<numOfNodes;jx++){
-                System.out.printf("%3d ", cost[ix][jx]);
+                System.out.printf("%03d ", cost[ix][jx]);
             }
             System.out.println();
         }
